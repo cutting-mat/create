d2fbe56e0b9b2aabfd7c364c13ec029e8dfeee8e
tower1229 <tower1229@foxmail.com>
2022-02-10 17:34:47 +0800

    同步目标那

--- template-element-ui/vue.config.js
+++ template-element-ui/vue.config.js
@@ -3,13 +3,13 @@
  * vue-cli配置
  * */ 
 module.exports = {
-    transpileDependencies: ['@tower1229/axios', '@tower1229/el-upload', '@tower1229/vue-store'],                                  // 需要babel编译的依赖包名
+    transpileDependencies: ['@cutting-mat/axios', '@cutting-mat/el-upload', '@cutting-mat/vue-store'],                                  // 需要babel编译的依赖包名
     css: {
         sourceMap: true                                         // 开启css map，方便调试
     },
     productionSourceMap: false,                                 // 生产环境关闭map
     integrity: process.env.NODE_ENV === 'production',           // 子资源完整性校验（SRI）
-    // outputDir: 'dist',                                          // 构建目录，默认'dist'
+    outputDir: 'dist',                                          // 构建目录，默认'dist'
     publicPath: process.env.NODE_ENV === 'production'           
     ? '/'                                                       // 生产环境构建路径，默认'/'
     : '/'      
